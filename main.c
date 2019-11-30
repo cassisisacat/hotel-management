789501a73488f46d054bb12df0ab07f97f9f18b1
João Silveira <54217440+jowlz@users.noreply.github.com>
2019-11-30 18:22:22 -0300

    Update com o início do projeto
    ~

--- main.c
+++ main.c
@@ -1,10 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 #include <string.h>
-
+#include <conio.h>
+struct Tcliente
+{
+    int codigo;
+    char nome[100];
+    float telefone;
+};
+typedef struct Tcliente cliente;
+cliente leCliente();
+void imprimeCliente(cliente m);
+void alteraCliente(cliente *m);
 int main()
 {
-    menu();
-    printf("Teste");
+    setlocale(LC_ALL,"portuguese");
+    cliente m[3];
+    int op,i;
+    do
+    {
+        system("cls");
+        printf("Escolha:\n");
+        printf("a - Cadastro Cliente\n");
+        printf("b - Alterar informações do cliente\n");
+        printf("c - Mostrar dados do cliente\n");
+        printf("d - Sair do programa.\n");
+        op=getchar();
+        switch (op)
+        {
+        case 'a':
+            for (i=0; i<3; i++)
+            {
+                m[i]=leCliente();
+            }
+            break;
+        case 'b':
+            for (i=0; i<3; i++)
+            {
+                alteraCliente(&m[i]);
+            }
+            break;
+        case 'c':
+            for (i=0; i<3; i++)
+            {
+                imprimeCliente(m[i]);
+            }
+            break;
+        }
+        if (op!='d')
+        {
+            system("pause");
+        }
+    }
+    while (op!='d');
+    return 0;
 }
 
+cliente leCliente()
+{
+    cliente m;
+    printf("Codigo.. ");
+    fflush(stdin);
+    scanf("%d", &m.codigo);
+    printf("Nome.. ");
+    fflush(stdin);
+    gets(m.nome);
+    printf("Telefone.. ");
+    fflush(stdin);
+    scanf("%f", &m.telefone);
+    return m;
+}
+void imprimeCliente(cliente m)
+{
+    printf("Código...: %d\n", m.codigo);
+    printf("Nome...: %s\n", m.nome);
+    printf("Telefone...: %.2f",  m.telefone);
+}
+void alteraCliente(cliente *m)
+{
+    printf("Codigo.. ");
+    fflush(stdin);
+    scanf("%d", &m->codigo);
+    printf("Nome.. ");
+    fflush(stdin);
+    gets(m->nome);
+    printf("Telefone.. ");
+    fflush(stdin);
+    scanf("%f", &m->telefone);
+}
