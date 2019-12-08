83daa25b7c619c9c43c49800fdaffee8b32b231a
João Silveira <54217440+jowlz@users.noreply.github.com>
2019-12-08 20:26:09 -0300

    Update management.h

--- management.h
+++ management.h
@@ -2,12 +2,9 @@
 
 void inclui_cliente(FILE *arquivo);
 void inclui_funcionario(FILE *arquivo);
-void cadastra_quarto(FILE *arq);
+void cadastrar_quarto(FILE *arq);
 int localiza_cliente(FILE *arquivo, int codigo);
 void imprime_cliente(FILE *arquivo);
 void imprime(FILE *arq, int pos, int tipo);
 void pesquisa(FILE *clientes, FILE *funcionarios);
-void exclui_estadia(FILE*arq_estadia, FILE *arq_quarto);
 void cadastrar_estadia(FILE *arq, FILE *arq_quartos);
-void imprimir_estadias_cliente(FILE*arq_estadia);
-
